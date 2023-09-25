738abfd1382664ade98e2a3b0dc9a76cab391b91
rabiaa5 <145457440+rabiaa5@users.noreply.github.com>
2023-09-25 16:01:48 +0300

    Add files via upload

---
+++ bolum1uygulama7/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace bolum1uygulama7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textboxMeyve = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.lboxMeyveler = new System.Windows.Forms.ListBox();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textboxMeyve
+            // 
+            this.textboxMeyve.Location = new System.Drawing.Point(70, 34);
+            this.textboxMeyve.Multiline = true;
+            this.textboxMeyve.Name = "textboxMeyve";
+            this.textboxMeyve.Size = new System.Drawing.Size(292, 34);
+            this.textboxMeyve.TabIndex = 1;
+            this.textboxMeyve.Text = "txtMeyve";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(70, 86);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(292, 75);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "sepete ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.Location = new System.Drawing.Point(70, 183);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(292, 75);
+            this.btnTemizle.TabIndex = 3;
+            this.btnTemizle.Text = "sepeti temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // lboxMeyveler
+            // 
+            this.lboxMeyveler.FormattingEnabled = true;
+            this.lboxMeyveler.Location = new System.Drawing.Point(420, 34);
+            this.lboxMeyveler.Name = "lboxMeyveler";
+            this.lboxMeyveler.Size = new System.Drawing.Size(183, 238);
+            this.lboxMeyveler.TabIndex = 4;
+            this.lboxMeyveler.Click += new System.EventHandler(this.lboxMeyveler_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(70, 275);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(292, 82);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "seçilen öğeyi sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.lboxMeyveler);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textboxMeyve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textboxMeyve;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.ListBox lboxMeyveler;
+        private System.Windows.Forms.Button btnSil;
+    }
+}
+
---
+++ bolum1uygulama7/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum1uygulama7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lboxMeyveler_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+              lboxMeyveler.Items.Add(textboxMeyve.Text);
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            lboxMeyveler.Items.Clear();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            lboxMeyveler.Items.Remove(lboxMeyveler.SelectedItem);
+        }
+    }
+}
